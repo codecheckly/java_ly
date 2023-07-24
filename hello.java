8980407c0fe0b0ecfe4e1d431bd78fc63ca4b68b
codecheckly <130332193+codecheckly@users.noreply.github.com>
2023-07-24 18:13:07 +0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+/**
+ * Hello world
+ *
+ */
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    String s = null;
+    System.out.println(s.length());
+  }
+
+}
